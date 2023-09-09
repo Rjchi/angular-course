30815a30a88d8a0d1b84b59c3987c3d3e5f029e9
Rjchi <richardospina18@gmail.com>
2023-09-09 14:41:51 -0500

    agregar comentarios del funcionamiento de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+/**-------------------------------------------------
+ * |  Aqui va el nombre del componente
+ * |  La plantilla HTML
+ * |  Y los estilos de propios de este componente
+ -------------------------------------------------*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/**-----------------------------------
+ * |  Esto engloba todo un componente
+ -----------------------------------*/
 import { AppComponent } from './app.component';
 
+/**-------------------------------------------------------------------------
+ * |  Aqui registramos todos los componentes que forman parte de este modulo
+ -------------------------------------------------------------------------*/
 @NgModule({
   declarations: [
     AppComponent
