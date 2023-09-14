4f8cde08f16e5582f2bd386313cfc5cd670391fc
Rjchi <richardospina18@gmail.com>
2023-09-14 16:09:26 -0500

    acceder al estado del formulario con formGroup

--- src/app/form-reactivo/form-reactivo.component.ts
+++ src/app/form-reactivo/form-reactivo.component.ts
@@ -6,17 +6,48 @@ import { Component } from '@angular/core';
 /**---------------------------------------------------------------------
 * |  Validators nos permite agregar reglas de validacion al formulario
 ---------------------------------------------------------------------*/
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-reactivo',
   templateUrl: './form-reactivo.component.html',
-  styleUrls: ['./form-reactivo.component.css']
+  styleUrls: ['./form-reactivo.component.css'],
 })
 export class FormReactivoComponent {
+  /**--------------------------------
+   * |  Asi accedemos a los inputs
+   --------------------------------*/
+  get name() {
+    return this.formUser.get('name') as FormControl;
+  }
+
+  get email() {
+    return this.formUser.get('email') as FormControl;
+  }
+
+  /**--------------------------------------------------------
+   * |  Asi podemos acceder al estado de nuestro formulario
+   --------------------------------------------------------*/
+  formUser = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+
+  Procesar = () => {
+    console.log(this.formUser.value);
+  };
+
+
+
+
+
+
+
+
+
   /**---------------------------------------------------------------------
    * |  Este va ha ser el estado inicial de los campos del formulario
    ---------------------------------------------------------------------*/
-  name = new FormControl('', Validators.required);
-  email = new FormControl('', [Validators.required, Validators.email]);
+  /*name = new FormControl('', Validators.required);
+  email = new FormControl('', [Validators.required, Validators.email]);*/
 }
