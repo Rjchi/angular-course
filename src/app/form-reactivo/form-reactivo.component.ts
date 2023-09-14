bb497d1352bf52a83b2208321e09c5fdc632e61c
Rjchi <richardospina18@gmail.com>
2023-09-14 15:42:34 -0500

    agregar reglas de validacion al formulario reactivo

--- src/app/form-reactivo/form-reactivo.component.ts
+++ src/app/form-reactivo/form-reactivo.component.ts
@@ -2,7 +2,11 @@
  * |  Formulario Reactivo
 ---------------------------*/
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+
+/**---------------------------------------------------------------------
+* |  Validators nos permite agregar reglas de validacion al formulario
+---------------------------------------------------------------------*/
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-reactivo',
@@ -13,6 +17,6 @@ export class FormReactivoComponent {
   /**---------------------------------------------------------------------
    * |  Este va ha ser el estado inicial de los campos del formulario
    ---------------------------------------------------------------------*/
-  name = new FormControl('');
-  email = new FormControl('');
+  name = new FormControl('', Validators.required);
+  email = new FormControl('', [Validators.required, Validators.email]);
 }
