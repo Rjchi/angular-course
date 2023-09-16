7ae6b9acc23a8cc44a7fe424a26a1e7a4f064aae
Rjchi <richardospina18@gmail.com>
2023-09-16 18:48:36 -0500

    emitir/escuchar eventos al componente padre

--- src/app/children/children.component.ts
+++ src/app/children/children.component.ts
@@ -3,15 +3,32 @@
  -----------------------------------------------------------------------------------*/
 import { Component, Input } from '@angular/core';
 
+/**----------------------------------------------------
+  * |  Con esto podemos crear un evento personalizado
+  * |   Y emitirlo con output
+  ---------------------------------------------------*/
+import { EventEmitter, Output } from '@angular/core';
+
 @Component({
   selector: 'app-children',
   templateUrl: './children.component.html',
-  styleUrls: ['./children.component.css']
+  styleUrls: ['./children.component.css'],
 })
 export class ChildrenComponent {
-
   /**------------------------------------------------------------
    * |  Debemos especificar cual es el parametro que esperamos
    ------------------------------------------------------------*/
   @Input() title?: string;
+
+  /**---------------------------------------------------------------
+   * |  Creamos un nuevo evento y lo emitimos al componente padre
+   ---------------------------------------------------------------*/
+  @Output() titleChange = new EventEmitter<string>();
+
+  /**-------------------------------------------------------------
+   * |  Le indicamos cuando queremos que se dispare el evento
+   -------------------------------------------------------------*/
+  emitTitleChange() {
+    this.titleChange.emit(this.title)
+  }
 }
